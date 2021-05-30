cfce8c8fca285ee8f9156e417996112aebf91fe7
Ricardo Queen <riqueen@gmail.com>
2021-05-30 00:23:51 -0300

    working example

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from backend import *
 # -- Set page config
-apptitle = 'Simulador Aerofólio'
+apptitle = 'Simulador Aerofólio - MECÂNICA UFRJ'
 
 st.set_page_config(page_title=apptitle, page_icon="🍃")
+
+images = ['./logos/Marca_Principal.png','./logos/logo_dem.jpg']
+
+cols = st.beta_columns(4)
+cols[0].image(images[0], use_column_width=True)
+cols[1].image(images[1], width=150)
+# st.image(images, height=200)
 
 # Title the app
 st.title('Simulador Aerofólio')
@@ -19,45 +26,43 @@ naca_foil = st.sidebar.text_input('Qual o perfil NACA será simulado?')
 
 # -- Create sidebar for plot controls
 st.sidebar.markdown('## Parâmetros do aerofólio')
-naca_points = st.sidebar.slider('Pontos na superfície do aerofólio', 100, 400, 200)  # min, max, default
+naca_points = st.sidebar.slider('Pontos na superfície do aerofólio', 10, 400, 200)  # min, max, default
 resolution = st.sidebar.slider('Resolução da malha', 50, 400, 50)
-length = st.sidebar.slider('Comprimento da tubulação (m)', 1.0, 8.0, 4.0)
-height = st.sidebar.slider('Altura do canal (m)', 0.5, 4.0, 1.0)
+length = st.sidebar.slider('Comprimento da tubulação (m)', 1.0, 8.0, 2.0)
+height = st.sidebar.slider('Altura do canal (m)', 0.5, 4.0, 2.0)
 flow_speed = st.sidebar.slider('Velocidade do escoamento do ar (m/s)', 10, 400, 200)
+circ_cc = st.sidebar.slider('Condição de Contorno no Aerofólio', 10, 400, flow_speed)
 speed_condition = float(flow_speed)#*float(height)
-
 alpha = st.sidebar.slider('Ângulo de ataque (Graus)', 0, 360, 5)
-
-# st.sidebar.markdown('#### Whitened and band-passed data')
-# whiten = st.sidebar.checkbox('Whiten?', value=True)
-# freqrange = st.sidebar.slider('Band-pass frequency range (Hz)', min_value=10, max_value=2000, value=(30,400))
-
-
-# # -- Create sidebar for Q-transform controls
-# st.sidebar.markdown('#### Q-tranform plot')
-# vmax = st.sidebar.slider('Colorbar Max Energy', 10, 500, 25)  # min, max, default
-# qcenter = st.sidebar.slider('Q-value', 5, 120, 5)  # min, max, default
-# qrange = (int(qcenter*0.8), int(qcenter*1.2))
+rho = st.sidebar.slider('Massa Específica do Ar (SI)', 0.8, 3.0, 1.292)
 
 if naca_foil != "":
     try:
-        naca_study = BladeMesh(profNaca=naca_foil, n_of_points_naca=naca_points, length=length, diameter=height,bc_upperwall_value=speed_condition, alpha=alpha)
+        naca_study = BladeMesh(profNaca=naca_foil, n_of_points_naca=naca_points, length=length, diameter=height,flow_speed=speed_condition, alpha=alpha, rho=rho)
         naca_study.create_mesh(mesh_resolution=resolution)
-        st.write('Malha Criada com Sucesso')
-        naca_study.create_formulation()
-        st.write('Formulação criada com sucesso')
+        naca_study.create_formulation(circ_cc=circ_cc)
         naca_study.compute_solution()
-        st.write('Solução computada com sucesso')
-        fig1, fig2 = naca_study.visualize_solutions('web')
+        fig1, fig2, fig3, fig4 = naca_study.visualize_solutions('web')
+        circ, lift, l1 = naca_study.calculate_lift()
         # naca_study.visualize_solutions(plot_type='matplotlib')
         with st.beta_expander("Visualizar Soluções Graficamente"):
             st.write(fig1)
             st.write(fig2)
+            st.write(fig3)
+            st.write(fig4)
         # print(mpld3.fig_to_html(fig1).split('</style>\n\n')[-1][0:200])
+        st.header('Teorema da força de sustentação:')
+        st.latex(r'''L = (\rho V)_{\infty} \Gamma ''')
+        st.markdown('Em que $\Gamma$ é a circulação no aerofólio, dada a condição de Kutta:')
+        st.latex(r"\Gamma =  \oint_{\text{Aerofólio}} \vec{V}\cdot d\vec{s}")
+        st.markdown('Para os dados definidos, tem-se:')
+        st.markdown(f'$\Gamma = {circ}$')
+        st.markdown(f'$L = {lift}N/m$ ')
     except Exception as e:
         if naca_foil == '':
             st.header('Por favor, inserir dados do aerofólio')
         else:
-            st.write('Deu ruim sô sepá nem existe esse naca ai', e)
+            st.write('Por favor, verifique se esse aerofólio é NACA e pertence à serie 4 ou 5.', e)
 else:
     st.header('Por favor, inserir dados do aerofólio')
+    
--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ from matplotlib import ticker
 from naca import return_naca_msh
 
 class BladeMesh():
-    def __init__(self, profNaca, n_of_points_naca, length, diameter,bc_upperwall_value, alpha = 5, delta=1, **kwargs):
+    def __init__(self, profNaca, n_of_points_naca, length, diameter,flow_speed, alpha = 5, delta=1, rho = 1.292, **kwargs):
         super().__init__(**kwargs)
         self.delta = delta # spacing between points
         self.length = length
@@ -21,7 +21,8 @@ class BladeMesh():
         self.nx = int(length/delta)
         self.ny = int(diameter/delta)
         self.alpha = alpha*np.pi/180
-        self.bc_upperwall_value = bc_upperwall_value
+        self.flow_speed = flow_speed
+        self.rho = rho
         return None
 
     def create_mesh(self, mesh_resolution = 50):
@@ -38,23 +39,31 @@ class BladeMesh():
         disloc_x = self.length/2 - baricenter_rotated[0]
         disloc_y = self.diameter/2 - baricenter_rotated[1]
         aerofoil = [Point(i[0] + disloc_x, i[1] + disloc_y) for i in aero_points_rotated]
+        self.original_baricenter = [baricenter[0], baricenter[1]]
         self.baricenter = [disloc_x + baricenter_rotated[0], disloc_y + baricenter_rotated[1]]
         aerofoil = Polygon(aerofoil)
         domain = Rectangle(p0, p1) - aerofoil
         self.mesh = generate_mesh(domain, mesh_resolution)
         return (200)
 
-    def create_formulation(self):
+    def create_formulation(self, circ_cc=None):
         #Creating Mesh according to geometry
         self.V = FunctionSpace(self.mesh, 'P', 1)
         # Define boundary condition expression
-        expr_str = str(self.bc_upperwall_value)+'*x[1]'
+        expr_str = str(self.flow_speed)+'*x[1]'
         # print(expr_str)
         self.u_D = Expression(expr_str, degree=1)
+        self.leading_edge_radius = 1.1019*(float(self.profNaca[-2:])/100)
+        if circ_cc is None:
+            self.circ_cc = 4*np.pi*self.flow_speed*self.leading_edge_radius*np.sin(self.alpha + abs(np.arcsin(self.original_baricenter[1]/self.leading_edge_radius)))*self.diameter
+            self.circ_cc = abs((self.circ_cc/2*np.pi)*np.log(self.leading_edge_radius))/4
+            print(self.circ_cc)
+        else:
+            self.circ_cc = circ_cc
         # Making a mark dictionary for bcs
         mark = {"generic": 0,"lower_wall": 1,"upper_wall": 2,"left": 3,"right": 4, "airfoil": 5 }
-        subdomains = MeshFunction("size_t", self.mesh, 1)
-        subdomains.set_all(mark["generic"])
+        self.subdomains = MeshFunction("size_t", self.mesh, 1)
+        self.subdomains.set_all(mark["generic"])
         length = self.length
         diameter = self.diameter
         class Left(SubDomain):
@@ -72,22 +81,22 @@ class BladeMesh():
         class Airfoil(SubDomain):
             def inside(self, x, on_boundary):
                 return on_boundary and not (near(x[1], 0) or near(x[1], diameter) or near(x[0], length) or near(x[0], 0))
-        # Marking the subdomains
+        # Marking the self.subdomains
         left = Left()
-        left.mark(subdomains, mark["left"])
+        left.mark(self.subdomains, mark["left"])
         right = Right()
-        right.mark(subdomains, mark["right"])
+        right.mark(self.subdomains, mark["right"])
         upper_wall = UpperWall()
-        upper_wall.mark(subdomains, mark["upper_wall"])
+        upper_wall.mark(self.subdomains, mark["upper_wall"])
         lower_wall = LowerWall()
-        lower_wall.mark(subdomains, mark["lower_wall"])
+        lower_wall.mark(self.subdomains, mark["lower_wall"])
         airfoil = Airfoil()
-        airfoil.mark(subdomains, mark["airfoil"])
-        bc_lower_wall = DirichletBC(self.V, 0, subdomains, mark["lower_wall"])
-        bc_upper_wall = DirichletBC(self.V, self.u_D((0,diameter)), subdomains, mark["upper_wall"])
-        bc_left = DirichletBC(self.V, self.u_D, subdomains, mark["left"])
-        bc_right = DirichletBC(self.V, self.u_D, subdomains, mark["right"])
-        bc_airfoil = DirichletBC(self.V, self.u_D((self.baricenter[0],self.baricenter[1])), subdomains, mark["airfoil"])
+        airfoil.mark(self.subdomains, mark["airfoil"])
+        bc_lower_wall = DirichletBC(self.V, 0, self.subdomains, mark["lower_wall"])
+        bc_upper_wall = DirichletBC(self.V, self.u_D((0,diameter)), self.subdomains, mark["upper_wall"])
+        bc_left = DirichletBC(self.V, self.u_D, self.subdomains, mark["left"])
+        bc_right = DirichletBC(self.V, self.u_D, self.subdomains, mark["right"])
+        bc_airfoil = DirichletBC(self.V, self.circ_cc, self.subdomains, mark["airfoil"])
         self.bcs = [bc_lower_wall, bc_upper_wall, bc_left, bc_right, bc_airfoil]
         # Define variational problem
         self.u_trial = TrialFunction(self.V)
@@ -105,15 +114,22 @@ class BladeMesh():
         self.vy = project(-self.u.dx(0), self.V)
         self.vvec = project(as_vector([self.vx, self.vy]),self.W)
         return (200)
-# def calculate_lift():
-
+    def calculate_lift(self):
+        ds = Measure("ds", domain=self.mesh, subdomain_data=self.subdomains)
+        GammaP = ds(5)
+        n = FacetNormal(self.mesh)
+        tangent = as_vector([n[1], -n[0]])
+        self.circ = -assemble((dot(self.vvec, tangent))*GammaP)
+        l1 = self.rho*self.vy*dot(self.vvec,n)*GammaP
+        lift=assemble(l1)
+        lift = self.rho*self.flow_speed*self.circ
+        return self.circ, lift, l1
     def visualize_solutions(self, plot_type):
         if not self.u:
             return('Please compute_solution() first!')
         else:
             aspect = 20
             pad_fraction = 0.5
-            rho = 1.292
             plt.figure(figsize=(12,12))
             ax = plt.gca()
             plt.title('Função Corrente \n $\\psi = {}$'.format(self.u_D((0,self.diameter))))
@@ -125,10 +141,7 @@ class BladeMesh():
             pad = axes_size.Fraction(pad_fraction, width)
             cax = divider.append_axes("right", size=width, pad=pad)
             plt.colorbar(im,cax=cax)
-            if plot_type == 'matplotlib':
-                plt.show()
-            elif plot_type == 'web':
-                fig1 = plt.gcf()
+            fig1 = plt.gcf()
             #fig2
             plt.figure(figsize=(12,12))
             plt.title('Velocidade vetorial ($\\vec{v}$) $[m/s]$ \n')
@@ -147,11 +160,34 @@ class BladeMesh():
             cb.locator = tick_locator
             cb.update_ticks()
             # plt.savefig('./Resultados/correnteaerovx.png', dpi=500, bbox_inches='tight')
-            if plot_type == 'matplotlib':
-                plt.show()
-            elif plot_type == 'web':
-                fig2 = plt.gcf()
-                return fig1, fig2
+            fig2 = plt.gcf()
+            plt.figure(figsize=(12,12))
+            plt.title('Velocidade em x ($v_x$) \n')
+            plt.xlabel('x $[m]$')
+            plt.ylabel('y $[m]$')
+            ax = plt.gca()
+            im = plot(self.u.dx(1))
+            divider = make_axes_locatable(ax)
+            width = axes_size.AxesY(ax, aspect=1./aspect)
+            pad = axes_size.Fraction(pad_fraction, width)
+            cax = divider.append_axes("right", size=width, pad=pad)
+            plt.colorbar(im, cax=cax)
+            fig3 = plt.gcf()
+            
+            plt.figure(figsize=(12,12))
+            plt.title('Velocidade em y ($v_y$) \n')
+            plt.xlabel('x $[m]$')
+            plt.ylabel('y $[m]$')
+            ax = plt.gca()
+            im = plot(-self.u.dx(0))
+            divider = make_axes_locatable(ax)
+            width = axes_size.AxesY(ax, aspect=1./aspect)
+            pad = axes_size.Fraction(pad_fraction, width)
+            cax = divider.append_axes("right", size=width, pad=pad)
+            plt.colorbar(im, cax=cax)
+            fig4 = plt.gcf()
+            
+            return fig1, fig2, fig3, fig4
 
 
 
